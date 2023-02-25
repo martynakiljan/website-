e21141ab13c18ed2890cfeebb27a5efb16158003
Martyna Kiljan <kmartynak@icloud.com>
2023-02-26 00:01:28 +0100

    some css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,14 +6,16 @@ import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__inner">
-        {" "}
-        <FontAwesomeIcon icon={faCopyright} className="footer-copyright" />
-        <p>2022</p>
-        <span className="footer-company-name"> Mat Bau</span>
-      </div>
-    </footer>
+    <>
+      <footer className="footer">
+        <div className="footer__inner">
+          {" "}
+          <FontAwesomeIcon icon={faCopyright} className="footer-copyright" />
+          <p>2022</p>
+          <span className="footer-company-name"> Mat Bau</span>
+        </div>
+      </footer>
+    </>
   );
 };
 
