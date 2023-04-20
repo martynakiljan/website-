994eaa35a15646fa361b49841c6aed53c5414dd0
Martyna Kiljan <kmartynak@icloud.com>
2023-04-20 14:16:14 +0200

    .

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,31 +2,36 @@
 import "./slider.scss";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
+import { FormattedMessage } from "react-intl";
 const Slider = () => {
   return (
-    <Carousel
-      autoPlay
-      showArrows={true}
-      className="section"
-      preventMovementUntilSwipeScrollTolerance={true}
-      swipeScrollTolerance={100}
-      infiniteLoop={true}
-      showThumbs={false}
-    >
-      <div className="slider__img slider__img-1"></div>
-      <div className="slider__img slider__img-2"></div>
-      <div className="slider__img slider__img-3"></div>
-      <div className="slider__img slider__img-4"></div>
-      <div className="slider__img slider__img-5"></div>
-      <div className="slider__img slider__img-6"></div>
-      <div className="slider__img slider__img-7"></div>
-      <div className="slider__img slider__img-8"></div>
-      <div className="slider__img slider__img-9"></div>
-      <div className="slider__img slider__img-10"></div>
-      <div className="slider__img slider__img-11"></div>
-      <div className="slider__img slider__img-12"></div>
-    </Carousel>
+    <>
+      <h2 className="title slider-title">
+        <FormattedMessage id="slider-title"></FormattedMessage>
+      </h2>
+      <Carousel
+        autoPlay
+        showArrows={true}
+        className="section"
+        preventMovementUntilSwipeScrollTolerance={true}
+        swipeScrollTolerance={100}
+        infiniteLoop={true}
+        showThumbs={false}
+      >
+        <div className="slider__img slider__img-1"></div>
+        <div className="slider__img slider__img-2"></div>
+        <div className="slider__img slider__img-3"></div>
+        <div className="slider__img slider__img-4"></div>
+        <div className="slider__img slider__img-5"></div>
+        <div className="slider__img slider__img-6"></div>
+        <div className="slider__img slider__img-7"></div>
+        <div className="slider__img slider__img-8"></div>
+        <div className="slider__img slider__img-9"></div>
+        <div className="slider__img slider__img-10"></div>
+        <div className="slider__img slider__img-11"></div>
+        <div className="slider__img slider__img-12"></div>
+      </Carousel>
+    </>
   );
 };
 
